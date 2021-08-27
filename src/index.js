55f7646a276f344470fdc41e33c4e4cef6622134
druloloy <dru.loloy@gmail.com>
2021-08-27 23:11:15 +0800

    Fixed copy button not working on mobile devices

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import '../src/scss/Main.scss';
 
+// Redirect the page to much secured protocol on page load
+function redirectHTTPS(){
+  const protocol = window.location.protocol;
+  const domain = window.location.domain;
+
+  if(protocol==="http:" && domain!==undefined) window.location = 'https://'+ domain;
+  else console.log("DEVELOPMENT MODE") // dev mode
+}
+window.addEventListener('load', redirectHTTPS);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
